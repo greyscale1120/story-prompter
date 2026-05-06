74d6023f00b7f970adea2c6139a2b40f7ede7e72
greyscale1120 <935968703@qq.com>
2026-05-06 04:06:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 setTimeout(function() {
-    alert("5秒到了");
     var fab = document.createElement("div");
     fab.setAttribute("style", "position:fixed!important;bottom:120px!important;right:16px!important;width:50px!important;height:50px!important;background:red!important;z-index:2147483647!important;border-radius:50%!important;color:white!important;font-size:20px!important;cursor:pointer!important;display:flex!important;align-items:center!important;justify-content:center!important;");
     fab.textContent = "✦";
     document.body.appendChild(fab);
-    alert("插入完成，右下角找红球");
-}, 5000);
+    alert("插入了");
+}, 15000);
