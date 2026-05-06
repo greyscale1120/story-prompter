51a9b99518f31e6ba9a2c1bb403ecf9fb821e8a0
greyscale1120 <935968703@qq.com>
2026-05-06 03:28:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 setTimeout(function() {
-    var fab = document.createElement("button");
-    fab.style.cssText = "position:fixed!important;bottom:120px!important;right:16px!important;width:50px!important;height:50px!important;background:red!important;z-index:2147483647!important;border-radius:50%!important;color:white!important;font-size:20px!important;border:none!important;cursor:pointer!important;display:flex!important;align-items:center!important;justify-content:center!important;";
-    fab.innerHTML = "✦";
-    document.body.appendChild(fab);
-    fab.onclick = function() { alert("在！"); };
+    alert("body子元素数量：" + document.body.children.length);
 }, 8000);
