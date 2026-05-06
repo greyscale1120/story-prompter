5134a5614e6628642aecb7a966c8f79e09d9581a
greyscale1120 <935968703@qq.com>
2026-05-06 03:44:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
-    var fab = document.createElement("button");
-    fab.setAttribute("style", "position:fixed!important;bottom:120px!important;right:16px!important;width:50px!important;height:50px!important;background:red!important;z-index:2147483647!important;border-radius:50%!important;color:white!important;font-size:20px!important;border:none!important;cursor:pointer!important;display:block!important;visibility:visible!important;opacity:1!important;");
+    var fab = document.createElement("div");
+    fab.setAttribute("style", "position:fixed!important;bottom:120px!important;right:16px!important;width:50px!important;height:50px!important;background:red!important;z-index:2147483647!important;border-radius:50%!important;color:white!important;font-size:20px!important;cursor:pointer!important;display:flex!important;align-items:center!important;justify-content:center!important;visibility:visible!important;opacity:1!important;");
     fab.textContent = "✦";
     document.body.appendChild(fab);
-    alert("插入完成");
+    alert("div插入完成");
 }, 8000);
