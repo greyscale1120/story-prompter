99f619c247474009e53d540ccebad4c6fb9d6886
greyscale1120 <935968703@qq.com>
2026-05-06 03:40:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     var fab = document.createElement("button");
-    fab.style.cssText = "position:fixed!important;bottom:120px!important;right:16px!important;width:50px!important;height:50px!important;background:red!important;z-index:2147483647!important;border-radius:50%!important;color:white!important;font-size:20px!important;border:none!important;cursor:pointer!important;";
-    fab.innerHTML = "✦";
+    fab.setAttribute("style", "position:fixed!important;bottom:120px!important;right:16px!important;width:50px!important;height:50px!important;background:red!important;z-index:2147483647!important;border-radius:50%!important;color:white!important;font-size:20px!important;border:none!important;cursor:pointer!important;display:block!important;visibility:visible!important;opacity:1!important;");
+    fab.textContent = "✦";
     document.body.appendChild(fab);
-    alert("插入了，现在有" + document.body.children.length + "个元素");
+    alert("插入完成");
 }, 8000);
